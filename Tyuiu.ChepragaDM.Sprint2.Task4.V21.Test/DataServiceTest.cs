573fab2da64cbe4c54065a3eaae6631e7ea13ad0
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-10-13 14:17:59 +0500

    Task4

---
+++ Tyuiu.ChepragaDM.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ChepragaDM.Sprint2.Task4.V21.Lib;
+namespace Tyuiu.ChepragaDM.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate ()
+        {
+            DataService ds = new DataService ();
+            double x = 1;
+            double y = 0;
+            var res = ds.Calculate (x, y);
+            Assert.AreEqual (0.986, res);
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint2.Task4.V21/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.ChepragaDM.Sprint2.Task4.V21.Lib;
+
+namespace Tyuiu.ChepragaDM.Sprint2.Task4.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции с      *");
+            Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+            Console.WriteLine("* переменной X, Y с клавиатуры, если x < y - 2, то                        *");
+            Console.WriteLine("*                                  2                                      *");
+            Console.WriteLine("*           x + 1    y            x  + 10                                 *");
+            Console.WriteLine("* z = (2 + ------- )   иначе -----------------                            *");
+            Console.WriteLine("*              2               2        2                                 *");
+            Console.WriteLine("*             x               y   - sinx  + 12                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой.             *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            double x, y;
+
+            Console.WriteLine("Введите значение переменной X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
