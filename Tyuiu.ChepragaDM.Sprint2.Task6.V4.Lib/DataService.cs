a1d2b475dc5cf1da91815ea187fa4518f549e5a6
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-10-13 15:23:03 +0500

    Task6

---
+++ Tyuiu.ChepragaDM.Sprint2.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ChepragaDM.Sprint2.Task6.V4.Lib
+{
+    public class DataService : ISprint2Task6V4
+    {
+        public string FindCardSuit(int value)
+        {
+            string res(int value) => value switch
+            {
+                1 => "пики",
+                2 => "трефы",
+                3 => "бубны",
+                4 => "червы",
+                _ => "Нет такой масти"
+
+            };
+            return res(value);
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint2.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ChepragaDM.Sprint2.Task6.V4.Lib;
+namespace Tyuiu.ChepragaDM.Sprint2.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardSuit()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.FindCardSuit(x);
+            Assert.AreEqual("трефы", res);
+        }
+    }
+}
