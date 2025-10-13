67015e26918e17affead6265646ebfe79039a6ea
DiagAA 69 <dima.prosto.2019@inbox.ru>
2025-10-13 14:53:37 +0500

    Task5

---
+++ Tyuiu.ChepragaDM.Sprint2.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ChepragaDM.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.ChepragaDM.Sprint2.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindMonthSeason()
+        {
+            DataService ds = new DataService();
+            int x = 12;
+            var res = ds.FindMonthSeason(x);
+            Assert.AreEqual("Зима", res);
+        }
+    }
+}
---
+++ Tyuiu.ChepragaDM.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.ChepragaDM.Sprint2.Task5.V2.Lib;
+
+namespace Tyuiu.ChepragaDM.Sprint2.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Чепрага Д. М. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Чепрага Дмитрий Максимович | ИБКСб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* По данному месяцу, определите в какую пору года попадает этот месяц     *");
+            Console.WriteLine("* (Зима, Лето, Весна, Осень).                                             *");
+            Console.WriteLine("*                                                                         *");
+                      
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            int x;
+
+            Console.WriteLine("Введите значение переменной X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+
+            string res = ds.FindMonthSeason(x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
